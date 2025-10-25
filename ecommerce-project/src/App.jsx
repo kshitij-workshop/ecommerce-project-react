5d36d549f04d935fb5df734380d3e7aa439993b1
Kshitij Kumar <kshitij.workshop@gmail.com>
2025-10-25 14:08:41 +0530

    Create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,32 +1,30 @@
-import axios from 'axios'
-import { Routes, Route } from 'react-router'
-import { useEffect, useState } from 'react'
-import {HomePage} from './pages/home/HomePage'
-import { CheckoutPage } from './pages/checkout/CheckoutPage'
-import { OrdersPage } from './pages/orders/OrdersPage'
-import './App.css'
+import axios from "axios";
+import { Routes, Route } from "react-router";
+import { useEffect, useState } from "react";
+import { HomePage } from "./pages/home/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
 
-  const [cart, setCart] = useState([])
+  const loadCart = async () => {
+    const response = await axios.get("/api/cart-items?expand=product");
+    setCart(response.data);
+  };
 
   useEffect(() => {
-    const fetchAppData = async () => {
-      const response = await axios.get('/api/cart-items?expand=product')
-      setCart(response.data)
-    }
-    fetchAppData()
-  },[])
-   
+    loadCart();
+  }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart}/>} />
-      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
-      <Route path='orders' element={<OrdersPage cart={cart} />} />
-
+      <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
